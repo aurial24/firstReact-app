1e25566fbe3e2bc92df3ac498a7c7c4cc8ead67a
aurial24 <electrix248@gmail.com>
2026-02-06 23:07:53 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./Message";
+import ListGroup from "./ListGroup.tsx";
+
+function App() {
+  return (
+    <div className="App">
+        <Message />
+        <ListGroup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ListGroup.tsx
@@ -0,0 +1,35 @@
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Fran'
+    ]
+
+    //Event Handler for mouse click
+    const handleClick = (event : React.MouseEvent<HTMLLIElement>) => console.log(event)
+    const handleItemClick = (item : string) => console.log("handleItemClick: " + item)
+    //
+    // if (items.length === 0) {
+    //     return (<>
+    //         <p>No items</p>
+    //     </>);
+    // }
+    return (
+        <>
+            <h1>Cities</h1>
+            {/*{items.length === 0 ? <p>No Cities</p> : null}*/}
+            <ul className="list-group">
+                {items.map( item =>
+                    <li
+                        className={`list-group-item`}
+                    key={item}
+                    onClick={() => handleItemClick(item)}
+                    >
+                        {item}
+                    </li>
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default ListGroup
